7e8dc0ba5d2fa389ab248b8d5d003f3caee27e60
bkcoder09 <80070457+bkcoder09@users.noreply.github.com>
2021-09-15 17:46:35 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,23 @@ function take_picture(){
 console.log('ml5 version:',ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_j9ZW9d4f/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log(modelLoaded);
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("person_name").innerHTML = results.label[0];
+        document.getElementById("person_accuracy").innerHTML = results.confidence[0].toFixed(3);
+    };
+}
